5b725a947d3d5112402e42c8f75566f78b56d976
Sherlock3721 <64225181+Sherlock3721@users.noreply.github.com>
2023-08-01 13:41:50 +0200

    Update Format.py

--- Format.py
+++ Format.py
@@ -1,49 +1,66 @@
-import glob, os, signal, sys, subprocess
-from subprocess import call
+import glob, os, sys
+from subprocess import call, run, Popen, PIPE
+from send2trash import send2trash as s2t
+from wakepy import keep
 
-Dir="/mnt/sftp/share/Disk2/Seriály"
-Del=True
+### Configure
+SearchDirectory     = "/mnt/sftp/share/Disk2/Filmy"
+DeleteAfterConvert  = False
+OnlyAnalyze         = False
+PowerOff            = False
 
-VC="libvpx-vp9"
-AC="aac"
-Extension="mp4"
-Preset="slow"
+VideoCodec          = "libvpx-vp9" 
+AudioCodec          = "libopus"
+Container           = "webm"
+# Preset="slow"
+CRF                 = "31"
+Deadline            = "good"   # good best realtime
 
-#LOG=" 2> " + Dir + "FFMEG.log"
+VideoSettings       = ["-map", "0:v", "-b:v", "0"]
+AudioSettings       = ["-ac", "2", "-b:a", "96k", "-map", "0:a:m:language:cze", "-map", "0:a?:m:language:eng"]
+SubtitleSettings    = ["-map", "0:s?:m:language:cze"]
+OtherSettings       = []
 
-SkipCodec=['h264','vp9','av1','avc1','aac','ac3','mjpeg','bin_data','mov_text','hevc','unknown','dvd_subtitle','mp3','opus','truehd','dts']
-SearchExtension=['avi','mp4','mkv','mpg','m4v','mov']
+SkipCodec           = ['h264','vp9','av1','aac','eac3','opus','flac','bin_data','mov_text','subrip','unknown','dvd_subtitle']
+SearchContainer     = ['ts','mkv','mov','avi']
+
+### DON'T TOUCH BELOW THIS TITLE
+
 files=[]
 TempNameUsed=False
 
+if not CRF == "":
+    OtherSettings += ["-crf", CRF]
+if not Deadline == "":
+    OtherSettings += ["-deadline", Deadline]
+
 def FFmpeg(FILE):
     if TempNameUsed:
-        NewFilename = os.path.splitext(FILE)[0].replace(".","",1) + "." + Extension
+        NewFilename = os.path.split(FILE)[0] + "/" + os.path.splitext(FILE)[0].split("/")[-1].replace(".","",1) + "." + Container
     else:
-        NewFilename = os.path.splitext(FILE)[0] + "." + Extension
-
-    Command=["ffmpeg", "-nostdin", "-i", FILE, "-n", "-c:v", VC, "-c:a", AC, "-crf", "23", "-preset", Preset, "-hide_banner", "-v", "error", "-movflags", "+faststart",  NewFilename ]
-
+        NewFilename = os.path.splitext(FILE)[0] + "." + Container
+    Command=["ffmpeg", "-nostdin", "-hide_banner", "-i", FILE, "-n", "-c:v", VideoCodec] + VideoSettings + ["-c:a", AudioCodec] + ["-row-mt", "1",  "-threads", "8", "-speed", "1", "-tile-columns", "6", "-frame-parallel", "1", "-auto-alt-ref", "1", "-lag-in-frames", "25", "-quality", "good"] + AudioSettings + SubtitleSettings + OtherSettings + [NewFilename]
+    print(' '.join(Command))
     try:
         if not os.path.exists(NewFilename):
             print("Starting formating file: " + os.path.split(FILE)[1])
-            subprocess.run(Command)
-            if Del == True:
-                print("Delete file!")
-                os.remove(File)
+            run(Command)
+            if DeleteAfterConvert:
+                print("Sending original file to trash!")
+                s2t(File)
             print("Done!")
             return True
         else:
             print("File already exists, skipping: " + FILE)
     except KeyboardInterrupt:
         print("Interupt inside formating! Deleting new file!")
-        os.remove(NewFilename)
+        s2t(NewFilename)
         sys.exit(0)
 
 def FFPROBE(FILE):
     cmnd = ['ffprobe', '-show_entries', 'stream=codec_name', '-of', 'default=noprint_wrappers=1:nokey=1', '-loglevel', 'quiet', FILE]
-    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err =  p.communicate()
+    p = Popen(cmnd, stdout=PIPE, stderr=PIPE)
+    out =  p.communicate()[0]
 
     codecs = out.decode().replace(r'\n', ' ').split()
 
@@ -51,25 +68,30 @@ def FFPROBE(FILE):
         if codec not in SkipCodec:
             print(FILE, codecs)
             return True
-
     return False
 
-while True:
+with keep.running() as k:
     try:
         print("Starting searching for files!")
-        for Ext in SearchExtension:
-            files.extend(glob.glob(Dir + '/**/*.' + Ext, recursive=True))
+        for Ext in SearchContainer:
+            files.extend(filter(os.path.isfile, glob.glob(SearchDirectory + '/**/*.' + Ext, recursive=True)))
+        files.sort(key = lambda x: os.stat(x).st_size, reverse=True)
+        print('\n'.join(files))
         print("Starting formating!")
-        for File in files:
-            if (File.endswith("." + Extension)):
-                if FFPROBE(File):
-                    TempFilename = os.path.split(File)[0] + "." + os.path.split(File)[1]
-                    os.rename(File, TempFilename)
-                    TempNameUsed=True
-                    FFmpeg(TempFilename)
-                continue
-            else:
-                FFmpeg(File)
+        if not OnlyAnalyze:
+            for File in files:
+                if (File.endswith("." + Container)):
+                    if FFPROBE(File):
+                        TempFilename = os.path.split(File)[0] + "/." + os.path.split(File)[1]
+                        print(TempFilename)
+                        os.rename(File, TempFilename)
+                        TempNameUsed=True
+                        FFmpeg(TempFilename)
+                    continue
+                else:
+                    FFmpeg(File)
     except KeyboardInterrupt:
         print('Interupt before formating!')
-        break
+        sys.exit(1)
+if PowerOff:
+    os.system('systemctl poweroff')
